78e82c7ec818cff31ec444ff435d250634058a8b
Samuel <33796679+hollowstrawberry@users.noreply.github.com>
2023-04-08 19:28:33 -0400

    🔎

--- PromptInspector.py
+++ PromptInspector.py
@@ -2,15 +2,25 @@ import io
 import os
 import asyncio
 from collections import OrderedDict
-from discord import Intents, Message, ApplicationContext, Attachment, File
+from discord import ApplicationContext, Intents, Message, Attachment, File, Embed, Member, RawReactionActionEvent
 from discord.ext import commands
 from dotenv import load_dotenv
 from PIL import Image
 
 load_dotenv()
 
-intents = Intents.default() | Intents.message_content | Intents.members
-client = commands.Bot(intents=intents)
+SCAN_LIMIT_BYTES = 10 * 1024**2
+SCAN_CHANNELS_FILENAME = "scan_channels.txt"
+INTENTS = Intents.default() | Intents.message_content | Intents.members
+
+if os.path.exists(SCAN_CHANNELS_FILENAME):
+    with open(SCAN_CHANNELS_FILENAME, 'r') as sf:
+        scan_channels = set(int(c) for c in sf.read().split('\n') if c.strip())
+else:
+    with open(SCAN_CHANNELS_FILENAME, 'w'):
+        scan_channels = set()
+
+bot = commands.Bot(intents=INTENTS, command_prefix="pi!")
 
 def read_info_from_image_stealth(image):
     width, height = image.size
@@ -25,7 +35,10 @@ def read_info_from_image_stealth(image):
     read_end = False
     for x in range(width):
         for y in range(height):
-            _, _, _, a = pixels[x, y]
+            try:
+                _, _, _, a = pixels[x, y]
+            except:
+                return None
             buffer += str(a & 1)
             if confirming_signature:
                 if index == len('stealth_pnginfo') * 8 - 1:
@@ -54,15 +67,44 @@ def read_info_from_image_stealth(image):
                 # impossible
                 read_end = True
                 break
-
             index += 1
         if read_end:
             break
     if sig_confirmed and binary_data != '':
-        # Convert binary string to UTF-8 encoded text
-        decoded_data = bytearray(int(binary_data[i:i + 8], 2) for i in range(0, len(binary_data), 8)).decode('utf-8',errors='ignore')
+        decoded_data = bytearray(int(binary_data[i:i + 8], 2) for i in range(0, len(binary_data), 8)).decode('utf-8', errors='ignore')
         return decoded_data
     return None
+
+def get_params_from_string(param_str):
+    output_dict = {}
+    parts = param_str.split('Steps: ')
+    prompts = parts[0]
+    params = 'Steps: ' + parts[1]
+    if 'Negative prompt: ' in prompts:
+        output_dict['Prompt'] = prompts.split('Negative prompt: ')[0]
+        output_dict['Negative Prompt'] = prompts.split('Negative prompt: ')[1]
+        if len(output_dict['Negative Prompt']) > 1000:
+            output_dict['Negative Prompt'] = output_dict['Negative Prompt'][:1000] + '...'
+    else:
+        output_dict['Prompt'] = prompts
+    if len(output_dict['Prompt']) > 1000:
+        output_dict['Prompt'] = output_dict['Prompt'][:1000] + '...'
+    params = params.split(', ')
+    for param in params:
+        try:
+            key, value = param.split(': ')
+            output_dict[key] = value
+        except ValueError:
+            pass
+    return output_dict
+
+def get_embed(embed_dict: dict, author: Member):
+    embed = Embed(title="Here's your image!", color=author.color)
+    for key, value in embed_dict.items():
+        embed.add_field(name=key, value=value, inline='Prompt' not in key)
+    pfp = author.avatar if author.avatar else author.default_avatar_url
+    embed.set_footer(text=f'Posted by {author.name}#{author.discriminator}', icon_url=pfp)
+    return embed
 
 async def read_attachment_metadata(i: int, attachment: Attachment, metadata: OrderedDict):
     print("Downloading", i)
@@ -77,7 +119,8 @@ async def read_attachment_metadata(i: int, attachment: Attachment, metadata: Ord
     else:
         print("Downloaded", i)
 
-@client.message_command(name="View Parameters")
+# Get raw list of parameters for every image in requested post
+@bot.message_command(name="View Parameters")
 async def message_command(ctx: ApplicationContext, message: Message):
     attachments = [a for a in message.attachments if a.filename.lower().endswith(".png")]
     if not attachments:
@@ -97,10 +140,67 @@ async def message_command(ctx: ApplicationContext, message: Message):
                 f.seek(0)
                 await ctx.respond(file=File(f, "parameters.yaml"), ephemeral=True)
     else:
-        await ctx.respond("This post contains no image generation data.\nTell the author to install [this extension](<https://github.com/ashen-sensored/sd_webui_stealth_pnginfo>).", ephemeral=True)
+        await ctx.respond(f"This post contains no image generation data.\nTell {ctx.author.mention} to install [this extension](<https://github.com/ashen-sensored/sd_webui_stealth_pnginfo>).", ephemeral=True)
 
-@client.event
+@bot.event
+async def on_message(message: Message):
+    # Scan images in allowed channels
+    if message.channel.id in scan_channels:
+        for i, attachment in enumerate(a for a in message.attachments if a.filename.lower().endswith(".png") and a.size < SCAN_LIMIT_BYTES):
+            metadata = OrderedDict()
+            await read_attachment_metadata(i, attachment, metadata)
+            if metadata:
+                await message.add_reaction('🔎')
+                return
+    # Owner commands
+    if not message.content.startswith('pi!') or not await bot.is_owner(message.author):
+        return
+    args = message.content.split('pi!')[1].split(' ')
+    if args[0] == "channeladd":
+        scan_channels.update(int(ch) for ch in args[1:])
+        with open(SCAN_CHANNELS_FILENAME, 'w') as f:
+            f.write('\n'.join([str(ch) for ch in scan_channels]))
+        await message.reply('✅')
+    elif args[0] == "channelremove":
+        scan_channels.difference_update(int(ch) for ch in args[1:])
+        with open(SCAN_CHANNELS_FILENAME, 'w') as f:
+            f.write('\n'.join([str(ch) for ch in scan_channels]))
+        await message.reply('✅')
+    elif args[0] == "channellist":
+        with open(SCAN_CHANNELS_FILENAME, 'r') as f:
+            ids = f.read().split("\n")
+        await message.reply('\n'.join([f'<#{int(i)}>' for i in ids if i.strip()]) or "*None*")
+
+
+# Send embed of parameters for first valid image in DMs
+@bot.event
+async def on_raw_reaction_add(ctx: RawReactionActionEvent):
+    if ctx.emoji.name == '🔎' and ctx.channel_id in scan_channels and not ctx.member.bot:
+        channel = bot.get_channel(ctx.channel_id)
+        message = await channel.fetch_message(ctx.message_id)
+        if not message:
+            return
+        attachments = [a for a in message.attachments if a.filename.lower().endswith(".png")]
+        if not attachments:
+            return
+        for i, attachment in enumerate(attachments):
+            metadata = OrderedDict()
+            await read_attachment_metadata(i, attachment, metadata)
+            if metadata:
+                embed = get_embed(get_params_from_string(metadata[0]), ctx.member)
+                embed.description = "You can also *right click a message -> Apps -> View Parameters*"
+                embed.set_thumbnail(url=attachment.url)
+                user_dm = await bot.get_user(ctx.user_id).create_dm()
+                await user_dm.send(embed=embed)
+                return
+        embed = get_embed({}, ctx.member)
+        embed.description = f"This post contains no image generation data.\nTell {ctx.member.mention} to install [this extension](<https://github.com/ashen-sensored/sd_webui_stealth_pnginfo>)."
+        embed.set_thumbnail(url=attachments[0].url)
+        user_dm = await bot.get_user(ctx.user_id).create_dm()
+        await user_dm.send(embed=embed)
+
+@bot.event
 async def on_ready():
-    print(f"Logged in as {client.user}!")
+    print(f"Logged in as {bot.user}!")
 
-client.run(os.environ["BOT_TOKEN"])
+bot.run(os.environ["BOT_TOKEN"])
