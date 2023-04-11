9165e44ae60ad2056f58297b3719774613e5df4b
derrian-distro <121591654+derrian-distro@users.noreply.github.com>
2023-04-10 21:33:28 -0400

    Add support for the modes I created
    Adds support for decoding alpha + compression, rgb, and rgb + compression

--- PromptInspector.py
+++ PromptInspector.py
@@ -1,3 +1,4 @@
+import gzip
 import io
 import os
 import asyncio
@@ -26,55 +27,111 @@ bot = commands.Bot(intents=INTENTS, command_prefix="pi!", auto_sync_commands=Fal
 
 
 def read_info_from_image_stealth(image):
+    # trying to read stealth pnginfo
     width, height = image.size
     pixels = image.load()
+
+    has_alpha = True if image.mode == 'RGBA' else False
+    mode = None
+    compressed = False
     binary_data = ''
-    buffer = ''
-    index = 0
+    buffer_a = ''
+    buffer_rgb = ''
+    index_a = 0
+    index_rgb = 0
     sig_confirmed = False
     confirming_signature = True
     reading_param_len = False
     reading_param = False
     read_end = False
-    if len(pixels[0, 0]) < 4:
-        return None
     for x in range(width):
         for y in range(height):
-            _, _, _, a = pixels[x, y]
-            buffer += str(a & 1)
+            if has_alpha:
+                r, g, b, a = pixels[x, y]
+                buffer_a += str(a & 1)
+                index_a += 1
+            else:
+                r, g, b = pixels[x, y]
+            buffer_rgb += str(r & 1)
+            buffer_rgb += str(g & 1)
+            buffer_rgb += str(b & 1)
+            index_rgb += 3
             if confirming_signature:
-                if index == len('stealth_pnginfo') * 8 - 1:
-                    if buffer == ''.join(format(byte, '08b') for byte in 'stealth_pnginfo'.encode('utf-8')):
+                if index_a == len('stealth_pnginfo') * 8:
+                    decoded_sig = bytearray(int(buffer_a[i:i + 8], 2) for i in
+                                            range(0, len(buffer_a), 8)).decode('utf-8', errors='ignore')
+                    if decoded_sig in {'stealth_pnginfo', 'stealth_pngcomp'}:
                         confirming_signature = False
                         sig_confirmed = True
                         reading_param_len = True
-                        buffer = ''
-                        index = 0
+                        mode = 'alpha'
+                        if decoded_sig == 'stealth_pngcomp':
+                            compressed = True
+                        buffer_a = ''
+                        index_a = 0
                     else:
                         read_end = True
                         break
+                elif index_rgb == len('stealth_pnginfo') * 8:
+                    decoded_sig = bytearray(int(buffer_rgb[i:i + 8], 2) for i in
+                                            range(0, len(buffer_rgb), 8)).decode('utf-8', errors='ignore')
+                    if decoded_sig in {'stealth_rgbinfo', 'stealth_rgbcomp'}:
+                        confirming_signature = False
+                        sig_confirmed = True
+                        reading_param_len = True
+                        mode = 'rgb'
+                        if decoded_sig == 'stealth_rgbcomp':
+                            compressed = True
+                        buffer_rgb = ''
+                        index_rgb = 0
             elif reading_param_len:
-                if index == 32:
-                    param_len = int(buffer, 2)
-                    reading_param_len = False
-                    reading_param = True
-                    buffer = ''
-                    index = 0
+                if mode == 'alpha':
+                    if index_a == 32:
+                        param_len = int(buffer_a, 2)
+                        reading_param_len = False
+                        reading_param = True
+                        buffer_a = ''
+                        index_a = 0
+                else:
+                    if index_rgb == 33:
+                        pop = buffer_rgb[-1]
+                        buffer_rgb = buffer_rgb[:-1]
+                        param_len = int(buffer_rgb, 2)
+                        reading_param_len = False
+                        reading_param = True
+                        buffer_rgb = pop
+                        index_rgb = 1
             elif reading_param:
-                if index == param_len:
-                    binary_data = buffer
-                    read_end = True
-                    break
+                if mode == 'alpha':
+                    if index_a == param_len:
+                        binary_data = buffer_a
+                        read_end = True
+                        break
+                else:
+                    if index_rgb >= param_len:
+                        diff = param_len - index_rgb
+                        if diff < 0:
+                            buffer_rgb = buffer_rgb[:diff]
+                        binary_data = buffer_rgb
+                        read_end = True
+                        break
             else:
                 # impossible
                 read_end = True
                 break
-            index += 1
         if read_end:
             break
     if sig_confirmed and binary_data != '':
-        decoded_data = bytearray(int(binary_data[i:i + 8], 2) for i in range(0, len(binary_data), 8)).decode('utf-8', errors='ignore')
-        return decoded_data
+        # Convert binary string to UTF-8 encoded text
+        byte_data = bytearray(int(binary_data[i:i + 8], 2) for i in range(0, len(binary_data), 8))
+        try:
+            if compressed:
+                decoded_data = gzip.decompress(bytes(byte_data)).decode('utf-8')
+            else:
+                decoded_data = byte_data.decode('utf-8', errors='ignore')
+            return decoded_data
+        except:
+            pass
     return None
 
 
